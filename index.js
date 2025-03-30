a32017691cc2cab8ca6cc6917aa38fe4342620f6
lu5w <mahmoodkargholy@gmail.com>
2025-03-30 06:40:14 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('btn1').addEventListener('click', function() {
+        alert('Button 1 clicked!');
+    });
+
+    document.getElementById('btn2').addEventListener('click', function() {
+        alert('Button 2 clicked!');
+    });
+
+    document.getElementById('btn3').addEventListener('click', function() {
+        alert('Button 3 clicked!');
+    });
+
+    document.getElementById('btn4').addEventListener('click', function() {
+        alert('Button 4 clicked!');
+    });
+
+    document.getElementById('btn5').addEventListener('click', function() {
+        alert('Button 5 clicked!');
+    });
+
+    document.getElementById('btn6').addEventListener('click', function() {
+        alert('Button 6 clicked!');
+    });
+
+    document.getElementById('hobbyBtn1').addEventListener('click', function() {
+        alert('Hobby Button 1 clicked!');
+    });
+
+    document.getElementById('hobbyBtn2').addEventListener('click', function() {
+        alert('Hobby Button 2 clicked!');
+    });
+
+    document.getElementById('hobbyBtn3').addEventListener('click', function() {
+        alert('Hobby Button 3 clicked!');
+    });
+
+    document.getElementById('hobbyBtn4').addEventListener('click', function() {
+        alert('Hobby Button 4 clicked!');
+    });
+
+    document.getElementById('hobbyBtn5').addEventListener('click', function() {
+        alert('Hobby Button 5 clicked!');
+    });
+
+    document.getElementById('hobbyBtn6').addEventListener('click', function() {
+        alert('Hobby Button 6 clicked!');
+    });
+});
